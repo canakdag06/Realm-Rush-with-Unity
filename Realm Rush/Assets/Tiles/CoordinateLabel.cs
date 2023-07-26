395e601dde13bfa8bb1293803345d0a4aff75f88
canakdag06 <canakdag06@gmail.com>
2023-07-26 22:03:56 +0300

    first commit

---
+++ Realm Rush/Assets/Tiles/CoordinateLabel.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways] // so we can run this script in edit mode
+[RequireComponent(typeof(TextMeshPro))]
+public class CoordinateLabel : MonoBehaviour
+{
+    [SerializeField] Color defaultColor = Color.white;
+    [SerializeField] Color blockedColor = Color.red;
+
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+    Waypoint waypoint;
+
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+        waypoint = GetComponentInParent<Waypoint>();
+        DisplayCoordinates(); // only on Awake() for 1 time because coordinates won't change when the game started
+    }
+    void Update()
+    {
+        if(!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+            label.enabled = true;
+        }
+
+        SetLabelColor();
+        ToggleLabels();
+    }
+
+    void ToggleLabels()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            label.enabled = !label.IsActive();
+        }
+    }
+
+    void SetLabelColor()
+    {
+        if(waypoint.IsPlaceable)
+        {
+            label.color = defaultColor;
+        }
+        else
+        {
+            label.color = blockedColor;
+        }
+    }
+
+    void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
